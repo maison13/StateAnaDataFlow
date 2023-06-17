7b14650df4fbd6d2b370e75eae0b7f7eac998c85
Just Vovo <mr.visitka@gmail.com>
2023-06-17 18:48:07 +0300

    Initial Commit

---
+++ StateAnaDataFlow/StateAnaDataFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateAnaDataFlowApp.swift
+//  StateAnaDataFlow
+//
+//  Created by Just Vovo on 17.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct StateAnaDataFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
